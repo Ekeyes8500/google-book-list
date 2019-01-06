a7106b5730d67b733531c061536f1c64a59c373f
Eric Keyes <keyes.eric@outlook.comgit config --global user.email keyes.eric@outlook.com>
2019-01-05 22:30:15 -0500

    first herok web build

---
+++ client/build/precache-manifest.e03ddd0fe5428de5f786539a2c57e661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e5d5dae44ec09dd843b7",
+    "url": "/static/js/main.e5d5dae4.chunk.js"
+  },
+  {
+    "revision": "2da5d9918dd65380d2d0",
+    "url": "/static/js/1.2da5d991.chunk.js"
+  },
+  {
+    "revision": "e5d5dae44ec09dd843b7",
+    "url": "/static/css/main.3416de57.chunk.css"
+  },
+  {
+    "revision": "eba0f92241aed452076f10a6d9956884",
+    "url": "/index.html"
+  }
+];
